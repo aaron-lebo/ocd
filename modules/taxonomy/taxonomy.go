4611263f4962b5e6b6a06d45773adbbef482e09b
Fisher Lebo <fisher.lebo@gmail.com>
2014-07-23 09:05:52 +0000

    rename Bag to Topics

--- modules/taxonomy/taxonomy.go
+++ modules/taxonomy/taxonomy.go
@@ -1,10 +1,10 @@
 package taxonomy
 
 type Taxonomy struct {
-	Topics Bag `xml:"http://purl.org/rss/1.0/modules/taxonomy/ topics"`
+	Topics Topics `xml:"http://purl.org/rss/1.0/modules/taxonomy/ topics"`
 }
 
-type Bag struct {
+type Topics struct {
 	Lis []Li `xml:"Bag>li"`
 }
 
